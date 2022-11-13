3a2778919ea316d5cc3c3b9d66688a3abe92d42b
马可 <78850005+makebl@users.noreply.github.com>
2022-11-13 19:49:56 +0800

    Update custom.sh

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -1,40 +1,43 @@
 #!/bin/bash
 
 shopt -s extglob
-rm -rf feeds/custom/{diy,mt-drivers,shortcut-fe,luci-app-mtwifi,base-files}
 
-for ipk in $(find feeds/custom/* -maxdepth 0 -type d);
+
+for ipk in $(find feeds/makebl/* -maxdepth 0 -type d);
 do
 	[[ "$(grep "KernelPackage" "$ipk/Makefile")" && ! "$(grep "BuildPackage" "$ipk/Makefile")" ]] && rm -rf $ipk || true
 done
 
-rm -rf package/{base-files,network/config/firewall,network/services/dnsmasq,network/services/ppp,system/opkg,libs/mbedtls}
+#<<'COMMENT'
 rm -Rf feeds/luci/{applications,collections,protocols,themes,libs,docs,contrib}
 rm -Rf feeds/luci/modules/!(luci-base)
-# rm -rf feeds/packages/libs/!(libev|c-ares|cjson|boost|lib*|expat|tiff|freetype|udns|pcre2)
 rm -Rf feeds/packages/!(lang|libs|devel|utils|net|multimedia)
-rm -Rf feeds/packages/multimedia/!(gstreamer1)
-rm -Rf feeds/packages/utils/!(pcsc-lite|xz)
+rm -Rf feeds/packages/multimedia/!(gstreamer1|ffmpeg)
+rm -Rf feeds/packages/libs/libcups
 rm -Rf feeds/packages/net/!(mosquitto|curl)
-rm -Rf feeds/base/package/{kernel,firmware}
+rm -Rf feeds/base/package/{firmware}
 rm -Rf feeds/base/package/network/!(services|utils)
 rm -Rf feeds/base/package/network/services/!(ppp)
-rm -Rf feeds/base/package/network/utils/!(iwinfo|iptables)
-rm -Rf feeds/base/package/utils/!(util-linux|lua)
-rm -Rf feeds/base/package/system/!(opkg|ubus|uci)
+rm -Rf feeds/base/package/system/!(opkg|ubus|uci|ca-certificates)
+rm -Rf feeds/base/package/kernel/!(cryptodev-linux)
+#COMMENT
+
+status=$(curl -H "Authorization: token $REPO_TOKEN" -s "https://api.github.com/repos/makebl/packages/actions/runs" | jq -r '.workflow_runs[0].status')
+while [ "$status" == "in_progress" ];do
+sleep 5
+status=$(curl -H "Authorization: token $REPO_TOKEN" -s "https://api.github.com/repos/makebl/packages/actions/runs" | jq -r '.workflow_runs[0].status')
+done
 
 ./scripts/feeds update -a
-./scripts/feeds install -a -p custom
+./scripts/feeds install -a -p makebl/packages -f
 ./scripts/feeds install -a
-./lang.sh
-sed -i 's/\(page\|e\)\?.acl_depends.*\?}//' `find package/feeds/custom/luci-*/luasrc/controller/* -name "*.lua"`
-sed -i 's/\/cgi-bin\/\(luci\|cgi-\)/\/\1/g' `find package/feeds/custom/luci-*/ -name "*.lua" -or -name "*.htm*" -or -name "*.js"` &
-sed -i 's/Os/O2/g' include/target.mk
-rm -rf ./feeds/packages/lang/golang
-svn co https://github.com/immortalwrt/packages/trunk/lang/golang feeds/packages/lang/golang
 
-sed -i '/root:/c\root:$1$tTPCBw1t$ldzfp37h5lSpO9VXk4uUE\/:18336:0:99999:7:::' package/feeds/custom/base-files/files/etc/shadow
-sed -i "s/tty1::askfirst/tty1::respawn/g" target/linux/*/base-files/etc/inittab
+rm -rf feeds/packages/lang/golang
+svn export https://github.com/coolsnowwolf/packages/trunk/lang/golang feeds/packages/lang/golang
+
+sed -i 's/\(page\|e\)\?.acl_depends.*\?}//' `find package/feeds/makebl/packages/luci-*/luasrc/controller/* -name "*.lua"`
+# sed -i 's/\/cgi-bin\/\(luci\|cgi-\)/\/\1/g' `find package/feeds/makebl/packages/luci-*/ -name "*.lua" -or -name "*.htm*" -or -name "*.js"` &
+sed -i 's/Os/O2/g' include/target.mk
 
 sed -i \
 	-e "s/+\(luci\|luci-ssl\|uhttpd\)\( \|$\)/\2/" \
@@ -42,10 +45,7 @@ sed -i \
 	-e 's/+python\( \|$\)/+python3/' \
 	-e 's?../../lang?$(TOPDIR)/feeds/packages/lang?' \
 	-e 's,$(STAGING_DIR_HOST)/bin/upx,upx,' \
-	package/feeds/custom/*/Makefile
-
-date=`(date +“%Y%m%d”)`
-sed -i -e "/\(# \)\?REVISION:=/c\REVISION:=$date" -e '/VERSION_CODE:=/c\VERSION_CODE:=$(REVISION)' include/version.mk
+	package/feeds/makebl/packages/*/Makefile
 
 cp -f devices/common/.config .config
 mv feeds/base feeds/base.bak
